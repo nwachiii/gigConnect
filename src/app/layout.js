3c6e90e40073a569f89f149a0dc8ffadaa255664
Josh <ukojoshy@gmail.com>
2024-03-04 17:20:33 +0100

    Chakra UI installation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,18 @@
-import {Inter} from 'next/font/google';
-import '../styles/globals.css';
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: 'Gig Connect App',
-	description: 'We connect businesses to top talents all around Africa',
+  title: "Gig Connect App",
+  description: "We connect businesses to top talents all around Africa",
 };
 
-export default function RootLayout({children}) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+export default function RootLayout({ children }) {
+  return (
+    <ChakraProvider>
+      <div className={inter.className}>{children}</div>
+    </ChakraProvider>
+  );
 }
